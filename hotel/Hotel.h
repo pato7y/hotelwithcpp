5603b16a2efd2246802eab571cb150f6b55309ac
pato7y <serhatyildiz721@gmail.com>
2024-04-10 11:30:46 +0300

    hotel.h created

---
+++ hotel/Hotel.h
@@ -0,0 +1,30 @@
+#ifndef HOTEL_H
+#define HOTEL_H
+
+#include <iostream>
+#include <string>
+
+class Hotel {
+private:
+    struct Node {
+        int id, date;
+        std::string name, roomtype;
+        Node* next;
+    };
+
+    Node* head;
+
+public:
+    Hotel(); 
+    ~Hotel(); 
+    void insert();
+    void search();
+    void update();
+    void del(); 
+    void show();
+    void sort();
+    void menu();
+};
+
+
+
