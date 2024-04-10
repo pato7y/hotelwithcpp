b421e825f69365af75427d2ab169d5ba251d664a
pato7y <serhatyildiz721@gmail.com>
2024-04-10 10:34:17 +0300

    main is created

---
+++ hotel/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namepspace std;
+Class Hotel
+{
+
+private:
+	struct node;
+	{
+		int id,date;
+		string name,roomtype;
+		node* next;
+		
+	};
+	
+	public:
+		node head=null;
+		void insert();
+		void delete();
+		void update();
+		void search();
+		void sort();
+		void show();
+		void menu();
+		void delete();
+		
+
+
+};
+void library::menu();
+{
+	p:
+		int choice;
+		cout<<"\n"<<endl;
+		cout<<"********welcome to hotel sero********"<<endl;
+		cout<<"1. allocate rooms\\t inserting a new room"<<endl;
+		cout<<"2. search rooms\\t searching rooms with room id"<<endl;
+		cout<<"3. update rooms\\t updating the room records"<<endl;
+		cout<<"4. delete rooms"<<endl;
+		cout<<"5. show room records"<<endl;
+	    cout<<"6. exit "<<endl;
+		
+	cout<<"enter the choice"<<endl;
+	cin>>choice;
+	cout<<"\n";
+	switch(choice)
+	{
+		
+		case 1:
+			
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
+int main(int argc, char** argv) {
+	return 0;
+}
